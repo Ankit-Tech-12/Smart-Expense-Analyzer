9b5065adb7026b0a97fd6ed1e76bb06c8413f4f4
Ankit Gupta <ankitgupta93727@gmail.com>
2026-09-22 14:11:51 +0530

    refactor: Remove localStorage utility functions and unused preloaded state in store configuration

--- client/src/App/store.js
+++ client/src/App/store.js
@@ -1,25 +1,21 @@
 import {configureStore} from "@reduxjs/toolkit"
 import expensesReducer from "../features/expenses/expensesSlice"
 import categoriesReducer from "../features/category/categoriesSlice"
-import { loadState, saveState } from "../utils/localStorage";
 import authReducer from "../features/auth/authSlice";
-
-const preloadedState = loadState();
 
 export const store=configureStore({
     reducer:{
         auth:authReducer,
         expenses:expensesReducer,
         categories: categoriesReducer, 
-    },
-     preloadedState,
+    }
 })
 
-// subscribe to store changes
-store.subscribe(() => {
-  saveState({
-    expenses: store.getState().expenses,
-    categories: store.getState().categories,
-  });
-});
+// // subscribe to store changes
+// store.subscribe(() => {
+//   saveState({
+//     expenses: store.getState().expenses,
+//     categories: store.getState().categories,
+//   });
+// });
 
